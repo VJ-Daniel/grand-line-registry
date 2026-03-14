04cbf2d4b992c1f0943d393e40e398cff2c287de
VJ Daniel Uy <vjdanieluy09@gmail.com>
2026-03-13 20:57:23 -0400

    fixed

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const express = require('express')
-const app = express()
-const port = 3000
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
